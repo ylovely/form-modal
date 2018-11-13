d36a0ab6408f05297880f3c74e0f4d29f88457cb
yeeyoung <yeeyoung57@gmail.com>
2018-11-14 07:59:57 +0800

    implementing map

---
+++ exercise-map-reducce.ts
@@ -0,0 +1,40 @@
+type T = string | number | boolean | object
+export function map(array: T[], iteratee: any): T[] {
+  return arrayMap(array, baseIteratee(iteratee));
+}
+
+export function arrayMap(array: T[], iteratee: any): T[] {
+  let index = -1;
+  const length = array.length;
+  const result = Array(length);
+
+  while (++index < length) {
+    result[index] = iteratee(array[index], index, array);
+  }
+  return result;
+}
+
+export function baseIteratee(value: any) {
+  if (typeof value === 'function') {
+    return value;
+  }
+  return property(value);
+}
+
+export function property(path: any) {
+  return baseProperty(toKey(path));
+}
+
+export function toKey(value: any): string {
+  if (typeof value === 'string') {
+    return value;
+  }
+  const result = (value + '');
+  return (result === '0' && (1 / value) === -(1 / 0)) ? '-0' : result;
+}
+
+export function baseProperty(key: string) {
+  return (object) => {
+    return object === null ? undefined : object[key];
+  };
+}
